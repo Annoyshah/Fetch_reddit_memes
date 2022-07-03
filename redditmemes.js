b39a5247890b29e63a343b2262e7c6c5658e5519
Amay shah <96513031+Annoyshah@users.noreply.github.com>
2022-07-03 12:38:16 +0530

    Add files via upload

--- redditmemes.js
+++ redditmemes.js
@@ -1,23 +1,26 @@
 var after=''
-window.onload=function fetchMemes(){
+function fetchMemes(){
+  
     if(document.getElementById('memes')){
         document.getElementById('memes').remove()
     }
     let parentdiv = document.createElement('div');
-parentdiv.id='memes'
+parentdiv.id='memes';
 fetch(`https://www.reddit.com/r/memes.json?after=${after}`)
 .then((apidata)=>{
 
     return apidata.json();
 })
 .then(body=>{
-    after=body.data.after
+    after=body.data.after;
     console.log(after);
     for(let i=0; i<body.data.children.length; i++){
         if(body.data.children[i].data.post_hint==='image'){
             let div=document.createElement('div')
+            div.id="mmholder"
             let h3=document.createElement('h3');
             let image=document.createElement('img');
+            image.id="imageholder"
             image.src=body.data.children[i].data.url_overridden_by_dest
             h3.textContent=body.data.children[i].title
             div.appendChild(h3);
